3e84948f58664c1b25e343ce41047b90ea612426
Artur Adib <arturadib@gmail.com>
2012-03-16 15:57:19 -0400

    debug() util

--- bootstrap/on_test.js
+++ bootstrap/on_test.js
@@ -1,32 +1,4 @@
-// Main Botio scripting module
 var botio = require('botio');
-
-// Import convenient Unix-like commands
 require('shelljs/global');
 
-function execWrap(cmd) {
-  echo('Running command:', cmd);
-
-  if (exec(cmd).code !== 0) {
-    echo('   ERROR!');
-    exit(1);
-  }
-};
-
-mkdir('-p', botio.private_dir);
-cd(botio.private_dir);
-
-execWrap('git clone '+botio.base_url+' .');
-execWrap('git checkout master');
-execWrap('git remote add requester '+botio.head_url);
-execWrap('git branch PR '+botio.head_sha);
-execWrap('git merge PR');
-
-exit(0);
-
-// echo('[botio] #### Bot.io the house!');
-// echo('[botio] This default script is configured to simply echo the contents of your repo.');
-
-// for (file in ls()) {
-//   echo(file);
-// }
+botio.cloneAndMerge();
