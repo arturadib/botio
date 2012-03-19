44aa83ea3a0312a8f4747dd15f2ae1b3268148bd
Artur Adib <arturadib@gmail.com>
2012-03-19 16:36:20 -0400

    static server

--- bootstrap/on_test.js
+++ bootstrap/on_test.js
@@ -1,15 +1,14 @@
 var botio = require('botio');
 require('shelljs/global');
 
-botio.message('#### Hello world');
-botio.message('Your Botio installation works! View full output for the list of files in this repo');
-
 botio.cloneAndMerge();
 
 echo('These are the files in your repo:');
-
 for (file in ls()) {
   echo('  '+file);
 }
 
-botio.cleanUp();
+botio.message('#### Hello world');
+botio.message('Your Botio installation works! View full output for the list of files in this repo');
+
+botio.cleanUp(); // make sure we don't leave stuff behind in the private dir
