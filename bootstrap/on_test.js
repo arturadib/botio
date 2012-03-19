3cedfa763db6c5c6b6786b033efdd5b4f7a64cc2
Artur Adib <arturadib@gmail.com>
2012-03-19 16:24:35 -0400

    !botio_message works

--- bootstrap/on_test.js
+++ bootstrap/on_test.js
@@ -2,7 +2,7 @@ var botio = require('botio');
 require('shelljs/global');
 
 botio.message('#### Hello world');
-botio.message('Your Botio installation works! View output details for the list of files in this repo');
+botio.message('Your Botio installation works! View full output for the list of files in this repo');
 
 botio.cloneAndMerge();
 
